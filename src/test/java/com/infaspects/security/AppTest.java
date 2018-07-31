c5290652c832c19fc03cf3c674897a0999006bbe
knihit <knihit@yahoo.com>
2018-07-30 23:19:50 -0400

    filter changes

--- src/test/java/com/infaspects/security/AppTest.java
+++ src/test/java/com/infaspects/security/AppTest.java
@@ -1,19 +1,13 @@
 package com.infaspects.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.impl.crypto.MacProvider;
 import org.junit.Test;
-
-import java.security.Key;
 
 import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
@@ -21,13 +15,13 @@ public class AppTest
     public void shouldAnswerWithTrue() {
         assertTrue(true);
 
-        Key key = MacProvider.generateKey();
+        /*Key key = MacProvider.generateKey();
         String compactJws = Jwts.builder()
                 .setSubject("Joe")
                 .signWith(SignatureAlgorithm.RS256, key)
                 .compact();
 
-        System.out.println( "Hello World!" );
-        Jwts.parser().setSigningKey(key).parseClaimsJws(compactJws).getBody().getSubject().equals("Joe");
+        System.out.println("Hello World!");
+        Jwts.parser().setSigningKey(key).parseClaimsJws(compactJws).getBody().getSubject().equals("Joe");*/
     }
 }
